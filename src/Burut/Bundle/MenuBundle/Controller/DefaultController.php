62af2dde547e747e7d1f494e755f0ae0862b3755
Burut <burut21@gmail.com>
2015-04-07 13:57:18 +0300

    added contacts from array

--- src/Burut/Bundle/MenuBundle/Controller/DefaultController.php
+++ src/Burut/Bundle/MenuBundle/Controller/DefaultController.php
@@ -22,22 +22,26 @@ class DefaultController extends Controller
         1 => [
             "title" => "Гречка",
             "price" => "10",
-            "img"   => "http://www.likar.info/pictures_ckfinder/images/grechka-po-kupecheski-retsept2.jpg"
+            "img"   => "http://www.likar.info/pictures_ckfinder/images/grechka-po-kupecheski-retsept2.jpg",
+            "comments" => "Гречка – отличная замена мясу, благодаря хорошо растворимым и усваиваемым белкам, а также калорийности и приятному вкусу."
         ],
         2 => [
             "title" => "Свечка",
             "price" => "20",
-            "img"   => "http://foto-ramki.com/predmety/clipart-svechki.png"
+            "img"   => "http://foto-ramki.com/predmety/clipart-svechki.png",
+            "comments" => "Герой романа «Свечка» Евгений Золоторотов — ветеринарный врач, московский интеллигент, прекрасный сын, муж и отец"
         ],
         4 => [
             "title" => "Хлеб",
             "price" => "40",
-            "img"   => "http://finansovoe-izobilie.ru/wp-content/uploads/hleb.jpg"
+            "img"   => "http://finansovoe-izobilie.ru/wp-content/uploads/hleb.jpg",
+            "comments" => "Хлеб — пищевой продукт, получаемый путём выпечки, паровой обработки или жарки теста, состоящего, как минимум, из муки и воды"
         ],
         7 => [
             "title" => "Колбаса",
             "price" => "70",
-            "img"   => "http://www.ua.all.biz/img/ua/catalog/98295.jpeg"
+            "img"   => "http://www.ua.all.biz/img/ua/catalog/98295.jpeg",
+            "comments" => "В детстве, бабушка в селе колбасу делала сама. Эта домашняя колбаса давно стала частью семейной традиции."
         ]
 
     ];
