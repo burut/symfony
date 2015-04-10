d0753dc587f29ce49ad49209a5756b5dffdf0159
Burut <burut21@gmail.com>
2015-04-10 23:30:39 +0300

    added contacts from array

--- src/Burut/Bundle/MenuBundle/Entity/Client.php
+++ src/Burut/Bundle/MenuBundle/Entity/Client.php
@@ -1,24 +1,34 @@
+<?php
+
+namespace Acme\StoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="product")
+ */
 class Product
 {
-/**
-* @ORM\Id
-* @ORM\Column(type="integer")
-* @ORM\GeneratedValue(strategy="AUTO")
-*/
-protected $id;
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
-/**
-* @ORM\Column(type="string", length=100)
-*/
-protected $name;
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
 
-/**
-* @ORM\Column(type="decimal", scale=2)
-*/
-protected $address;
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    protected $address;
 
-/**
-* @ORM\Column(type="text")
-*/
-protected $phone;
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $phone;
 }
