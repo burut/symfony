87c8ad9c9bd6e6c426e5ac47b2971d835b37f2a3
Burut <burut21@gmail.com>
2015-05-25 13:10:10 +0300

    !

--- src/App/JoboardBundle/Tests/Utils/JoboardTest.php
+++ src/App/JoboardBundle/Tests/Utils/JoboardTest.php
@@ -8,23 +8,20 @@ use App\JoboardBundle\Utils\Joboard;
 
 class JoboardTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSlugify($test)
-    {
-        if (empty($text)) {
-            return 'n-a';
-        }
-        $this->assertEquals('company', Joboard::slugify('Company'));
-        $this->assertEquals('ooo-company', Joboard::slugify('ooo company'));
-        $this->assertEquals('company', Joboard::slugify(' company'));
-        $this->assertEquals('company', Joboard::slugify('company '));
-        $this->assertEquals('n-a', Joboard::slugify(''));
-        $this->assertEquals('n-a', Joboard::slugify(' - '));
-        $this->assertEquals('developpeur-web', Joboard::slugify('Développeur Web'));
-        if (empty($text))
+
+        public function testSlugify()
         {
-            return 'n-a';
+            static public function slugify($text)
+        {
+            if (empty($text)) {
+                return 'n-a';
+            }
+        }
+            $this->assertEquals('company', Joboard::slugify('Company'));
+            $this->assertEquals('ooo-company', Joboard::slugify('ooo company'));
+            $this->assertEquals('company', Joboard::slugify(' company'));
+            $this->assertEquals('company', Joboard::slugify('company '));
+            $this->assertEquals('n-a', Joboard::slugify(''));
         }
 
-        return $text;
-    }
 }
